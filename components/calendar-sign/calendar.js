86e66e45d80bdffeb1f98e5357c58072890dc5ef
qibin.lqb <qibin.lqb@alibaba-inc.com>
2016-06-22 17:17:18 +0800

    clean code

--- components/calendar-sign/calendar.js
+++ components/calendar-sign/calendar.js
@@ -44,7 +44,7 @@ var calUtil = {
     $("#"+calUtil.calendarId).html(str);
     // calendar header
     var calendarName=calUtil.showYear+"年"+calUtil.showMonth+"月";
-    $(".calendar_month_span").html(calendarName);  
+    $(".calendar_month_span").html(calendarName);
   },
   // bind event
   bindEnvent:function(){
@@ -80,8 +80,8 @@ var calUtil = {
         calUtil.showMonth=parseInt(nowMonth)-1;
         if(calUtil.showMonth==0)
         {
-            calUtil.showMonth=12;
-            calUtil.showYear-=1;
+          calUtil.showMonth=12;
+          calUtil.showYear-=1;
         }
         break;
       case "next":
@@ -92,51 +92,51 @@ var calUtil = {
         calUtil.showMonth=parseInt(nowMonth)+1;
         if(calUtil.showMonth==13)
         {
-            calUtil.showMonth=1;
-            calUtil.showYear+=1;
+          calUtil.showMonth=1;
+          calUtil.showYear+=1;
         }
         break;
     }
   },
   // return day in month
   getDaysInmonth : function(iMonth, iYear){
-   var dPrevDate = new Date(iYear, iMonth, 0);
-   return dPrevDate.getDate();
+    var dPrevDate = new Date(iYear, iMonth, 0);
+    return dPrevDate.getDate();
   },
   bulidCal : function(iYear, iMonth) {
-   var aMonth = new Array();
-   aMonth[0] = new Array(7);
-   aMonth[1] = new Array(7);
-   aMonth[2] = new Array(7);
-   aMonth[3] = new Array(7);
-   aMonth[4] = new Array(7);
-   aMonth[5] = new Array(7);
-   aMonth[6] = new Array(7);
-   var dCalDate = new Date(iYear, iMonth - 1, 1);
-   var iDayOfFirst = dCalDate.getDay();
-   var iDaysInMonth = calUtil.getDaysInmonth(iMonth, iYear);
-   var iVarDate = 1;
-   var d, w;
-   aMonth[0][0] = "日";
-   aMonth[0][1] = "一";
-   aMonth[0][2] = "二";
-   aMonth[0][3] = "三";
-   aMonth[0][4] = "四";
-   aMonth[0][5] = "五";
-   aMonth[0][6] = "六";
-   for (d = iDayOfFirst; d < 7; d++) {
-    aMonth[1][d] = iVarDate;
-    iVarDate++;
-   }
-   for (w = 2; w < 7; w++) {
-    for (d = 0; d < 7; d++) {
-     if (iVarDate <= iDaysInMonth) {
-      aMonth[w][d] = iVarDate;
+    var aMonth = new Array();
+    aMonth[0] = new Array(7);
+    aMonth[1] = new Array(7);
+    aMonth[2] = new Array(7);
+    aMonth[3] = new Array(7);
+    aMonth[4] = new Array(7);
+    aMonth[5] = new Array(7);
+    aMonth[6] = new Array(7);
+    var dCalDate = new Date(iYear, iMonth - 1, 1);
+    var iDayOfFirst = dCalDate.getDay();
+    var iDaysInMonth = calUtil.getDaysInmonth(iMonth, iYear);
+    var iVarDate = 1;
+    var d, w;
+    aMonth[0][0] = "日";
+    aMonth[0][1] = "一";
+    aMonth[0][2] = "二";
+    aMonth[0][3] = "三";
+    aMonth[0][4] = "四";
+    aMonth[0][5] = "五";
+    aMonth[0][6] = "六";
+    for (d = iDayOfFirst; d < 7; d++) {
+      aMonth[1][d] = iVarDate;
       iVarDate++;
-     }
     }
-   }
-   return aMonth;
+    for (w = 2; w < 7; w++) {
+      for (d = 0; d < 7; d++) {
+        if (iVarDate <= iDaysInMonth) {
+          aMonth[w][d] = iVarDate;
+          iVarDate++;
+        }
+      }
+    }
+    return aMonth;
   },
   // if is today
   ifToday : function(day, month, year){
@@ -144,77 +144,77 @@ var calUtil = {
   },
   // if is signed
   ifSigned : function(signList, day, month, year){
-   var signed = false;
-   $.each(signList,function(index,item){
-    if (item.signDate) {
-      var sDate=item.signDate; 
-      if (typeof sDate==="number" || typeof sDate==="string") {
-        try{
-          sDate=new Date(sDate);
-        }catch(e){
-          console.log("fmt Date error", e);
+    var signed = false;
+    $.each(signList,function(index,item){
+      if (item.signDate) {
+        var sDate=item.signDate;
+        if (typeof sDate==="number" || typeof sDate==="string") {
+          try{
+            sDate=new Date(sDate);
+          }catch(e){
+            console.log("fmt Date error", e);
+          }
         }
-      }
-      var y=sDate.getFullYear();
-      var m=sDate.getMonth() + 1;
-      var d=sDate.getDate();
-      if (y == year && m == month && d == day) {
+        var y=sDate.getFullYear();
+        var m=sDate.getMonth() + 1;
+        var d=sDate.getDate();
+        if (y == year && m == month && d == day) {
+          signed = true;
+          return false;
+        }
+      }else if(item.signDay == day) {
         signed = true;
         return false;
       }
-    }else if(item.signDay == day) {
-     signed = true;
-     return false;
-    }
-   });
-   return signed;
+    });
+    return signed;
   },
   // draw calendar
   drawCal : function(iYear, iMonth ,signList) {
-   var myMonth = calUtil.bulidCal(iYear, iMonth);
+    var myMonth = calUtil.bulidCal(iYear, iMonth);
     var classPrev = calUtil.disablePrev?'calendar_month_prev disable':'calendar_month_prev';
     var classNext = calUtil.disableNext?'calendar_month_next disable':'calendar_month_next';
-   var htmls = new Array();
-   htmls.push("<div class='sign_main' id='sign_layer'>");
-   htmls.push("<div class='sign_succ_calendar_title'>");
-   htmls.push("<div class='"+classNext+"'>下月</div>");
-   htmls.push("<div class='"+classPrev+"'>上月</div>");
-   htmls.push("<div class='calendar_month_span'></div>");
-   htmls.push("</div>");
-   htmls.push("<div class='sign' id='sign_cal'>");
-   htmls.push("<table>");
-   htmls.push("<tr>");
-   htmls.push("<th>" + myMonth[0][0] + "</th>");
-   htmls.push("<th>" + myMonth[0][1] + "</th>");
-   htmls.push("<th>" + myMonth[0][2] + "</th>");
-   htmls.push("<th>" + myMonth[0][3] + "</th>");
-   htmls.push("<th>" + myMonth[0][4] + "</th>");
-   htmls.push("<th>" + myMonth[0][5] + "</th>");
-   htmls.push("<th>" + myMonth[0][6] + "</th>");
-   htmls.push("</tr>");
-   var d, w;
-   for (w = 1; w < 7; w++) {
+    var htmls = new Array();
+    htmls.push("<div class='sign_main' id='sign_layer'>");
+    htmls.push("<div class='sign_succ_calendar_title'>");
+    htmls.push("<div class='"+classNext+"'>下月</div>");
+    htmls.push("<div class='"+classPrev+"'>上月</div>");
+    htmls.push("<div class='calendar_month_span'></div>");
+    htmls.push("</div>");
+    htmls.push("<div class='sign' id='sign_cal'>");
+    htmls.push("<table>");
     htmls.push("<tr>");
-    for (d = 0; d < 7; d++) {
-     var ifSigned = calUtil.ifSigned(signList, myMonth[w][d], iMonth, iYear);
-     var ifToday = calUtil.ifToday(myMonth[w][d], iMonth, iYear);
-     var tdClass = "";
-     if(ifToday) {
-      tdClass+="today ";
-     }
-     if(ifSigned){
-      tdClass+="on ";
-     } else {
-      //
-     }
-     htmls.push("<td class='"+tdClass+"'>" + (!isNaN(myMonth[w][d]) ? myMonth[w][d] : " ") + "</td>");
-    }
+    htmls.push("<th>" + myMonth[0][0] + "</th>");
+    htmls.push("<th>" + myMonth[0][1] + "</th>");
+    htmls.push("<th>" + myMonth[0][2] + "</th>");
+    htmls.push("<th>" + myMonth[0][3] + "</th>");
+    htmls.push("<th>" + myMonth[0][4] + "</th>");
+    htmls.push("<th>" + myMonth[0][5] + "</th>");
+    htmls.push("<th>" + myMonth[0][6] + "</th>");
     htmls.push("</tr>");
-   }
-   htmls.push("</table>");
-   htmls.push("</div>");
-   htmls.push("</div>");
-   return htmls.join('');
+    var d, w;
+    for (w = 1; w < 7; w++) {
+      htmls.push("<tr>");
+      for (d = 0; d < 7; d++) {
+        var ifSigned = calUtil.ifSigned(signList, myMonth[w][d], iMonth, iYear);
+        var ifToday = calUtil.ifToday(myMonth[w][d], iMonth, iYear);
+        var tdClass = "";
+        if(ifToday) {
+          tdClass+="today ";
+        }
+        if(ifSigned){
+          tdClass+="on ";
+        } else {
+          //
+        }
+        htmls.push("<td class='"+tdClass+"'>" + (!isNaN(myMonth[w][d]) ? myMonth[w][d] : " ") + "</td>");
+      }
+      htmls.push("</tr>");
+    }
+    htmls.push("</table>");
+    htmls.push("</div>");
+    htmls.push("</div>");
+    return htmls.join('');
   },
   // set disable pre button
   setdisablePrev: function (flag) {
